314924024afa363d0c7663271cb6689f225bf2ff
Christoph <captainsikay@gmail.com>
2020-04-16 19:25:32 +0200

    Add random operator selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { attacker, defender } from './operator'
+import { Operator } from './operator.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  att: Array<Operator> = attacker;
+  def: Array<Operator> = defender;
+
+  operator: Operator = attacker[0];
+
+  onRandomSelect(category) {
+    if (category === 'attacker') {
+      for (let i = 0; i < 20; i++) {
+        const randomOperator = Math.floor(Math.random() * this.att.length);
+        setTimeout(() => {
+          this.operator = this.att[randomOperator];
+        }, 100 * i);
+      }
+    }
+
+    if (category === 'defender') {
+      for (let i = 0; i < 20; i++) {
+        const randomOperator = Math.floor(Math.random() * this.def.length);
+        setTimeout(() => {
+          this.operator = this.att[randomOperator];
+        }, 100 * i);
+      }
+    }
+  }
 }
---
+++ src/app/operator.model.ts
@@ -0,0 +1,5 @@
+export class Operator {
+  name: string;
+  img: string;
+  icon: string;
+}
